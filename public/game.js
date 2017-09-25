a127d07885998355c954462f70f359bb3f9e0844
AnuarBJJ <anuar.zhukenov@gmail.com>
2017-09-25 13:00:26 -0700

    created pong class

---
+++ public/game.js
@@ -0,0 +1,29 @@
+class Pong {
+	constructor () {
+		this.length = 400;
+		this.height = 300;
+		this.ball = {
+			x: 200,
+			y: 200
+		}
+		this.direction = {
+			xAxis: Math.floor(Math.random() * 5 + 1),
+			yAxis: Math.floor(Math.random() * 5),
+		}
+	}
+	moveBall () {
+		const interval = setInterval(() => {
+			this.ball = {
+				x: this.ball.x + this.direction.xAxis,
+				y: this.ball.y + this.direction.yAxis
+			}
+			if (this.ball.y === 0 || this.ball.y === this.height) {
+				this.direction.yAxis = - this.direction.yAxis;
+			}
+		}, 10);
+
+	}
+
+}
+
+module.exports.Pong = Pong;
